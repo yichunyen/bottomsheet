a2e4cc6ba9398a7c11cef5ef134681b403d6ffe1
Yi-Chun <yenyichun.developer@gmail.com>
2020-01-27 23:16:55 +0800

    feature: implemented default bottom sheet layout
    Signed-off-by: Yi-Chun <yenyichun.developer@gmail.com>

--- app/src/main/java/yichunyen/demo/bottomsheets/MainActivity.kt
+++ app/src/main/java/yichunyen/demo/bottomsheets/MainActivity.kt
@@ -1,16 +1,19 @@
 package yichunyen.demo.bottomsheets
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private var sheetBehavior: BottomSheetBehavior<View>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initBottomBehavior()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -19,6 +22,36 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.menu_default -> {
+                displayDefaultBottomSheet()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun initBottomBehavior() {
+        // CoordinatorLayout layout
+        sheetBehavior = BottomSheetBehavior.from(llBottomSheet)
+        sheetBehavior!!.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(p0: View, p1: Float) {
+
+            }
+
+            override fun onStateChanged(p0: View, p1: Int) {
+            }
+
+        })
+    }
+
+    private fun displayDefaultBottomSheet() {
+        if (sheetBehavior!!.state != BottomSheetBehavior.STATE_EXPANDED) {
+            llBottomSheet.visibility = View.VISIBLE
+            sheetBehavior!!.state = BottomSheetBehavior.STATE_EXPANDED
+        } else {
+            llBottomSheet.visibility = View.GONE
+            sheetBehavior!!.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
     }
 }
