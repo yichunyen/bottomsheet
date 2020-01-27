7db4c29afecf9e26a5aa344b764536ac3e0d094b
Yi-Chun <yenyichun.developer@gmail.com>
2020-01-27 19:27:28 +0800

    MainActivity: added the menu to support to display different bottom sheets settings
    Signed-off-by: Yi-Chun <yenyichun.developer@gmail.com>

--- app/src/main/java/yichunyen/demo/bottomsheets/MainActivity.kt
+++ app/src/main/java/yichunyen/demo/bottomsheets/MainActivity.kt
@@ -2,11 +2,23 @@ package yichunyen.demo.bottomsheets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
