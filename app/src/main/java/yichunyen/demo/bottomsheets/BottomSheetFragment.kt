a17b7f9b166998aeb27186b5dd977de55d5266c2
Yi-Chun <yenyichun.developer@gmail.com>
2020-01-29 14:08:32 +0800

    Added BottomSheetFragment.

---
+++ app/src/main/java/yichunyen/demo/bottomsheets/BottomSheetFragment.kt
@@ -0,0 +1,26 @@
+package yichunyen.demo.bottomsheets
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.layout_bottom_sheet, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() = BottomSheetFragment()
+    }
+}
